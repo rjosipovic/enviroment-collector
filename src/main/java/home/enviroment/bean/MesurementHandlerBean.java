ccb53f4bc339ceff566f9d3afab874c7ebdb1c01
roman <roman@stark.localdomain>
2018-03-10 13:43:43 +0100

    Initial commit; logging changed, getLastmesurements method added

---
+++ src/main/java/home/enviroment/bean/MesurementHandlerBean.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home.enviroment.bean;
+
+import home.enviroment.entity.Mesurement;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author roman
+ */
+@Stateless
+public class MesurementHandlerBean {
+
+    private static final Logger LOGGER = Logger.getLogger(MesurementHandlerBean.class.getName());
+    
+    @PersistenceContext(unitName = "home.enviromentPU")
+    private EntityManager em;
+    
+    @PostConstruct
+    private void construct() {
+        LOGGER.log(Level.FINER, "MesurementHandlerBean created");
+    }
+    
+    public void storeMesurements(String temperature, String presure, String humidity) {
+        Mesurement mesurement = new Mesurement();
+        mesurement.setMesureTime(new Date());
+        mesurement.setTemperature(Float.parseFloat(temperature));
+        mesurement.setPresure(Float.parseFloat(presure));
+        mesurement.setHumidity(Float.parseFloat(humidity));
+        em.persist(mesurement);
+    }
+    
+    public List<Mesurement> getAllMesurments() {
+        return em.createNamedQuery("getAllMesurments").getResultList();
+    }
+    
+    public List<Mesurement> getlLastMesurements(int limit) {
+    	return em.createNamedQuery("getAllMesurments").setMaxResults(limit).getResultList();
+    }
+    
+    public List<Mesurement> getLastHourMesurments() {
+        Query q = em.createNamedQuery("getLastHourMesurments");
+        Calendar c = Calendar.getInstance();
+        Date end = c.getTime();
+        c.add(Calendar.HOUR, -1);
+        Date start = c.getTime();
+
+        q.setParameter("start", start);
+        q.setParameter("end", end);
+        return q.getResultList();
+    }
+    
+    @PreDestroy
+    private void remove()  {
+        LOGGER.log(Level.FINER, "Removing MesurementHandlerBean");
+    }
+}
---
+++ src/main/java/home/enviroment/service/EnviromentCollectorBean.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home.enviroment.service;
+
+import home.enviroment.bean.MesurementHandlerBean;
+import home.enviroment.entity.Mesurement;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author roman
+ */
+@Path(value = "collector")
+@Stateless
+public class EnviromentCollectorBean {
+
+    private static final Logger LOGGER = Logger.getLogger(EnviromentCollectorBean.class.getName());
+    
+    @EJB
+    private MesurementHandlerBean mesurementHandlerBean;
+    
+    @PostConstruct
+    private void construct() {
+        LOGGER.log(Level.FINER, "EnviromentCollectorBean created");
+    }
+    
+    @GET
+    public String monitor() {
+        return "Home EnviromentCollector is running";
+    }
+    
+    @GET
+    @Path("mesure")
+    public String mesure(
+            @QueryParam("temperature")String temperature,
+            @QueryParam("presure")String presure,
+            @QueryParam("humidity")String humidity) {
+        String logMsg = MessageFormat.format("Temp: {0}, Presure: {1}, Humidity: {2}", temperature, presure, humidity);
+        LOGGER.log(Level.INFO, logMsg);
+        mesurementHandlerBean.storeMesurements(temperature, presure, humidity);
+        return "mesurement recieved";
+    }
+    
+    @GET
+    @Path("getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllMesurments() {
+    	LOGGER.log(Level.INFO, "Entering getAllMesurements ...");
+        List<Mesurement> allMesurments = mesurementHandlerBean.getAllMesurments();
+        return Response.ok(allMesurments).build();
+    }
+
+    @GET
+    @Path("getLast")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLastMesurments(@QueryParam("limit")int limit) {
+    	String logMsg = MessageFormat.format("Enetering getLastMesurements, limit={0}", limit);
+    	LOGGER.log(Level.INFO, logMsg);
+        List<Mesurement> limitMesurments = mesurementHandlerBean.getlLastMesurements(limit);
+        return Response.ok(limitMesurments).build();
+    }
+    
+    @GET
+    @Path("getLast")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLastMesurements() {
+    	LOGGER.log(Level.INFO, "Entering getLastMesurements ...");
+        List<Mesurement> lastHourMesurements = mesurementHandlerBean.getLastHourMesurments();
+        return Response.ok(lastHourMesurements).build();    	
+    }
+    
+    @PreDestroy
+    private void remove() {
+        LOGGER.log(Level.FINER, "Removing EnviromentCollectorBean");
+    }
+}
